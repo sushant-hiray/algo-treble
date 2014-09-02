e92e0923f84ed6e00d370c388fadd2a848084f4c
Sushant Hiray <hiraysushant@gmail.com>
2014-09-02 17:15:46 +0530

    Fixed Bug in 276A

--- codeforces/276A.cpp
+++ codeforces/276A.cpp
@@ -9,11 +9,17 @@ int main()
 	ll n, k, f, t, max, temp;
 	cin >> n >> k;
 	max = 0;
-	for(ll i=0; i < n; i++)
+	cin >> f >> t;
+	if (t > k)
+		max = f - t + k;
+	else
+		max = f;
+	
+	for(ll i=1; i < n; i++)
 	{
 		cin >> f >> t;
 		if (t > k)
-			temp = f - t - k;
+			temp = f - t + k;
 		else
 			temp = f;
 		if (temp > max)
