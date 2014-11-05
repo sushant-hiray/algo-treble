a0301e0ed5a42fc06b4596f0f36c8cbb5a5dedd8
sushant <hiraysushant@gmail.com>
2014-11-05 14:19:36 +0530

    generate exponential graph working

--- placements/worksap/orienteering/orienteering.cpp
+++ placements/worksap/orienteering/orienteering.cpp
@@ -4,10 +4,12 @@
 #include <cstring>
 #include <list>
 #include <queue>
+#include <cmath>
 #define INT_MAX 1000000009
 using namespace std;
 
 struct edge;
+struct checkpoint_edge;
 
 struct location {
     int x;
@@ -29,6 +31,7 @@ struct location {
 
 
 struct node {
+    int id;
     int wt;
     int val;
     location l;
@@ -42,6 +45,7 @@ struct node {
         wt = INT_MAX;
     }
     void print() {
+        cout << wt << " | ";
         l.print();
     }
     void reset() {
@@ -65,13 +69,69 @@ struct edge
 };
 
 
+struct checkpoint_node
+{
+    int cp_id; 
+    int node_id;
+    int wt;
+    bool visited;
+    string nid_str;
+    list<checkpoint_edge*> neighbors;
+    checkpoint_node(int c,int n, int m, int w=INT_MAX) {
+        cp_id = c;
+        node_id = n;
+        wt = w;
+        visited = false;
+        string str(m,'0');
+        int i=m-1;
+        while(i>=0) {
+            str[i] = ((n & 1) ? '1' : '0');
+            n = n >> 1;
+            i--;
+        }
+        nid_str = str;
+    }
+
+    void reset() {
+        visited = false;
+        wt = INT_MAX;
+    }
+
+    bool eq(checkpoint_node n) {
+        return (cp_id == n.cp_id && node_id == n.node_id);
+    }
+
+    void print() {
+        cout << wt << " | " << cp_id << ": " << nid_str;
+    }
+
+    bool is_valid() {
+        return (nid_str[cp_id]=='1' ? true : false);
+    }
+};
+
+struct checkpoint_edge
+{
+    int weight;
+    checkpoint_node* a;
+    checkpoint_edge(checkpoint_node* _a, int wt) {
+        a = _a;
+        weight = wt;
+    }
+};
 
 class node_comparison
 {
 public:
+    
     bool operator() (const node* lhs, const node* rhs) const
     {
-        return (lhs->wt < rhs->wt);
+        return (lhs->wt > rhs->wt);
+    }
+
+    bool operator() (const checkpoint_node* lhs, const checkpoint_node* rhs) const
+    {
+        return (lhs->wt > rhs->wt);
     }
 };
 
@@ -79,18 +139,26 @@ class Orienteering
 {
 private:
     vector<vector<node*> > maze;
+    vector<vector<checkpoint_node*> > cp_nodes;
     vector<location> checkpoint;
     vector<vector<int> > weights;
     int width;
     int height;
+    int cp_s;
     void parse_input();
     void print_index(int);
     void print_maze();
     void print_cp();
     void init_graph();
-    void reset_graph();
     void calc_weights();
-    int shortest_path(location a, location b);
+    void generate_cp_graph();
+
+    template<typename T>
+    int shortest_path(T*, T* );
+    
+    template<typename T>  
+    void reset_graph(vector<vector<T*>> &);
+    
 public:
     Orienteering();
     void main();
@@ -107,8 +175,9 @@ void Orienteering::main()
     parse_input();
     init_graph();
     print_maze();
+    calc_weights();
+    generate_cp_graph();
     print_cp();
-    cout << shortest_path(location(2,1), location(2,3)) << endl;
     
 }
 
@@ -120,7 +189,6 @@ void Orienteering::parse_input()
         for (int j = 0; j < width; j++) {
             char ch;
             cin >> ch;
-            cout << ch;
             switch(ch) {
                 case '#' : {
                     node* n = new node(-1,i,j);
@@ -152,7 +220,17 @@ void Orienteering::parse_input()
             }
         }
         maze.push_back(t);
-        cout << endl;
+    }
+
+    cp_s = checkpoint.size();
+    for(int i=0;i<cp_s;i++) {
+        weights.push_back(vector<int>(cp_s, INT_MAX));
+        vector<checkpoint_node*> temp;
+        for(int j=0;j<pow(2, cp_s);j++) {
+            checkpoint_node* cp_n = new checkpoint_node(i,j, cp_s);
+            temp.push_back(cp_n);
+        }
+        cp_nodes.push_back(temp);
     }
 }
 
@@ -203,6 +281,26 @@ void Orienteering::print_cp()
         cout << " ";
     }
     cout << endl;
+    for(int i=0;i<checkpoint.size();i++) {
+        for(int j=0;j<checkpoint.size();j++) {
+            cout << weights[i][j] << " ";
+        }
+    cout << endl;
+    }
+
+
+    for(int i=0;i<cp_nodes.size();i++) {
+        for(int j=0;j<cp_nodes[i].size();j++) {
+            cout << "{";
+            cp_nodes[i][j]->print();
+            cout << "--->";
+            for(auto it = cp_nodes[i][j]->neighbors.begin(); it!= cp_nodes[i][j]->neighbors.end(); it++) {
+                ((*it)->a)->print();
+                cout << ", ";
+            }
+            cout << "}"<<endl;
+        }
+    }
 }
 
 
@@ -231,32 +329,49 @@ void Orienteering::init_graph()
     }
 }
 
-void Orienteering::reset_graph()
+void Orienteering::generate_cp_graph()
 {
-    for(int i=0;i<height;i++) {
-        for(int j=0;j<width;j++) {
-            maze[i][j]->reset();
+    for(int i=0; i<cp_s;i++) {
+        for(int j=0; j < pow(2,cp_s);j++) {
+            if (cp_nodes[i][j]->is_valid()) {
+                string str = cp_nodes[i][j]->nid_str;
+                for(int k=0;k <cp_s;k++) {
+                    if (str[k]=='0' && i!=k) {
+                        int new_id = j + pow(2, cp_s - 1 - k);
+                        checkpoint_edge* e = new checkpoint_edge(cp_nodes[k][new_id], weights[i][k]);
+                        (cp_nodes[i][j]->neighbors).push_back(e);
+                    } 
+                }
+            }
         }
     }
 }
 
+template<typename T>
+void Orienteering::reset_graph(vector<vector<T*>>& graph)
+{
+    for(int i=0;i<graph.size();i++) {
+        for(int j=0;j<graph[i].size();j++) {
+            graph[i][j]->reset();
+        }
+    }
+}
 
-int Orienteering::shortest_path(location a, location b) {
-    node * end = maze[b.x][b.y];
-    node * cur = maze[a.x][a.y];
+template<typename T>
+int Orienteering::shortest_path(T* cur, T* end) {
     cur->wt=0;
-    priority_queue<node*, std::vector<node*>, node_comparison> pq;
+    priority_queue<T*, std::vector<T*>, node_comparison> pq;
     pq.push(cur);
     while(!pq.empty()) {
         cur = pq.top();
         cur->visited=true;
         if (cur->eq(*end)) {
-            return cur->val;
+            return cur->wt;
         } else {
             pq.pop();
-            for(list<edge*>::iterator it = cur->neighbors.begin(); it!=cur->neighbors.end();it++) {
-                if ((*it)->a->wt >  cur->wt + 1) {
-                    (*it)->a->wt = cur->wt+1;
+            for(auto it = cur->neighbors.begin(); it!=cur->neighbors.end();it++) {
+                if ((*it)->a->wt >  cur->wt + (*it)->weight) {
+                    (*it)->a->wt = cur->wt+(*it)->weight;
                     pq.push((*it)->a);
                 }
             }
@@ -265,6 +380,22 @@ int Orienteering::shortest_path(location a, location b) {
     return INT_MAX;
 }
 
+void Orienteering::calc_weights()
+{
+    for(int i=0;i<checkpoint.size();i++) {
+        for(int j=0;j<checkpoint.size();j++) {
+            if (i==j)
+                weights[i][j]=0;
+            else if (j < i)
+                weights[i][j] = weights[j][i];
+            else {
+                reset_graph<node>(maze);
+                weights[i][j] = shortest_path<node>(maze[checkpoint[i].x][checkpoint[i].y], 
+                                maze[checkpoint[j].x][checkpoint[j].y]);
+            }
+        }
+    }
+}
 
 int main(int argc, char const *argv[])
 {
